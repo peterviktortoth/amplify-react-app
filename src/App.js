863f87b094fa80c077354654738cc1e9effd8b18
peterviktortoth <152824719+peterviktortoth@users.noreply.github.com>
2023-12-09 14:22:42 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -5,28 +5,63 @@ function App() {
   const [results, setResults] = useState([]);
   const [error, setError] = useState('');
 
+  const getCurrentLocation = async (apiKey) => {
+    try {
+      const position = await getCurrentPosition();
+      
+      if (position) {
+        const { latitude, longitude } = position.coords;
+
+        const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?key=${apiKey}&q=${latitude}+${longitude}`);
+        
+        if (!response.ok) {
+          throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
+        }
+        
+        const data = await response.json();
+
+        if (data.results && data.results.length > 0) {
+          console.log('Location details:', data.results[0]);
+          return [latitude, longitude];
+        } else {
+          throw new Error('Location not found in OpenCage response');
+        }
+      } else {
+        throw new Error('Failed to retrieve geolocation');
+      }
+    } catch (error) {
+      console.error(`Error getting location: ${error.message}`);
+      return null;
+    }
+  };
+  
+  const getCurrentPosition = () => {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => resolve(position),
+        (error) => reject(error)
+      );
+    });
+  };
+
   const getRentalPrices = async (radius) => {
     try {
-      // Get current location coordinates
-      const coordinates = await getCurrentLocation();
+      const coordinates = await getCurrentLocation("7cc33eaee3bf43c480474df135a0b6b8");
 
       if (coordinates) {
         const [latitude, longitude] = coordinates;
         console.log(`Current Location: Latitude ${latitude}, Longitude ${longitude}`);
 
-        // Use the obtained coordinates in the RentCast API request
         const url = `https://api.rentcast.io/v1/listings/rental/long-term?latitude=${latitude}&longitude=${longitude}&radius=${radius}&status=Active&limit=500`;
 
         const headers = {
           "accept": "application/json",
-          // Include your RentCast API key here
           "X-Api-Key": "5fe60f24d1c04d22a89cc5e1583a119f",
         };
 
         const response = await fetch(url, { headers });
         const data = await response.json();
 
-        // Extract rental prices and bedroom counts from the properties
         const rentalPrices = {};
         data.forEach(property => {
           const bedrooms = property.bedrooms || "Unknown";
@@ -39,7 +74,6 @@ function App() {
           }
         });
 
-        // Display average rental prices for each bedroom count
         const averagePrices = Object.entries(rentalPrices).map(([bedrooms, prices]) => {
           const averagePrice = prices.reduce((sum, price) => sum + price, 0) / prices.length;
           return { bedrooms, average_price: averagePrice };
@@ -58,35 +92,21 @@ function App() {
     }
   };
 
-  const getCurrentLocation = async () => {
-    try {
-      const response = await fetch('https://ipinfo.io');
-      const data = await response.json();
-      return data.loc.split(',');
-    } catch (error) {
-      console.error(`Error getting location: ${error}`);
-      return null;
-    }
-  };
-
   const calculateRentalPrices = () => {
-    // Validate input
     if (isNaN(radius) || radius <= 0) {
       setError('Please enter a valid positive number for the radius.');
-      setResults([]); // Clear previous results
+      setResults([]);
       return;
     } else {
-      setError(''); // Clear error message
+      setError('');
     }
 
-    // Call the JavaScript function equivalent to the Python logic
     getRentalPrices(parseFloat(radius));
   };
 
   useEffect(() => {
-    // Call the API when the component mounts
     calculateRentalPrices();
-  }, []); // Empty dependency array ensures the effect runs once on mount
+  }, []);
 
   return (
     <div>
@@ -112,7 +132,7 @@ function App() {
         <ul>
           {results.map((item) => (
             <li key={item.bedrooms}>
-              {item.bedrooms}: ${item.average_price.toFixed(2)}
+              {item.bedrooms === "Unknown" ? "Studio" : item.bedrooms}: ${item.average_price.toLocaleString('en-US', { maximumFractionDigits: 2 })}
             </li>
           ))}
         </ul>
